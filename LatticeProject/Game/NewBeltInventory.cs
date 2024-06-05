125fcb7a1629dcc01c1519d27effbecf1496653a
TastyGold <tastygold99513@gmail.com>
2024-06-05 22:48:31 +1000

    5/06 NewBeltInventory.cs methods

--- LatticeProject/Game/NewBeltInventory.cs
+++ LatticeProject/Game/NewBeltInventory.cs
@@ -14,9 +14,17 @@ namespace LatticeProject.Game
         public float trailingDistance = 0; //distance from last item to tail
         public const float minItemDistance = 2 / 3f; //minimum distance items can be between themselves
 
+        public float totalBeltLength = 0;
+
         public void AddToHead(int itemId, float distanceFromHead)
         {
-            if (items.First is null) items.AddFirst(new BeltInventoryElement(itemId, 1, 0));
+            if (items.First is null)
+            {
+                //if there are no items on the belt, creates a new rle element
+                items.AddFirst(new BeltInventoryElement(itemId, 0, 1));
+                leadingDistance = distanceFromHead;
+                trailingDistance = totalBeltLength - distanceFromHead;
+            }
             else
             {
                 float newItemPosition = distanceFromHead; //the position on the belt the new item will be set to
@@ -30,27 +38,58 @@ namespace LatticeProject.Game
                     newFirstDistance = minItemDistance;
                 }
 
-                float newFirstDistance = leadingDistance - newItemPosition;
-
                 if (items.First.Value.ItemDistanceMatches(itemId, newFirstDistance))
                 {
+                    //increases the quantity of the first RLE chain on the belt
                     items.First.Value.count++;
                 }
                 else
                 {
-                    items.AddFirst(new BeltInventoryElement(itemId, 1, newFirstDistance));
+                    //creates a new RLE and inserts it at the start of the belt
+                    items.AddFirst(new BeltInventoryElement(itemId, newFirstDistance, 1));
                 }
 
                 leadingDistance = newItemPosition;
             }
+        }
+
+        public void MoveItems(float distance)
+        {
+            //moves all items on the belt by increasing the distance from the head (start) of the belt to the first item
+            leadingDistance += distance;
+            trailingDistance -= distance;
+        }
+
+        public void RemoveLast()
+        {
+            if (items.Last is not null)
+            {
+                if (items.Last.Value.count > 1)
+                {
+                    //if last rle chain has more than 1 element, decreases rle chain count
+                    items.Last.Value.count--;
+                    trailingDistance += items.Last.Value.distance;
+                }
+                else
+                {
+                    //else removes the last rle chain
+                    items.RemoveLast();
+                }
+            }
+            else throw new Exception("Cannot remove item from empty belt inventory.");
+        }
+
+        public void Clear()
+        {
+            items.Clear();
         }
     }
 
     internal class BeltInventoryElement
     {
         public int itemId;
-        public int count;
         public float distance;
+        public int count;
 
         public bool ItemDistanceMatches(int itemId, float distance)
         {
@@ -58,11 +97,11 @@ namespace LatticeProject.Game
                 && this.distance == distance;
         }
 
-        public BeltInventoryElement(int itemId, int count, float distance)
+        public BeltInventoryElement(int itemId, float distance, int count)
         {
             this.itemId = itemId;
-            this.count = count;
             this.distance = distance;
+            this.count = count;
         }
     }
 }
